8d08ad8b5563f0e0d69402abdcb881265119429f
SinoharaRyuga <raitan3170@gmail.com>
2022-07-08 13:44:35 +0900

    地雷で全てのセルが埋められるようにした

--- Assets/MineSweeper/Cell.cs
+++ Assets/MineSweeper/Cell.cs
@@ -1,17 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// セルの状態を管理する　
+/// 自身のセルを開ける 
+/// </summary>
 public class Cell : MonoBehaviour
 {
-    [SerializeField] Text _view = null;
-    [SerializeField] CellState _cellState = CellState.None;
+    [SerializeField, Tooltip("地雷の数を表示するテキスト")] Text _view = null;
+    [SerializeField, Tooltip("セルの状態")] CellState _cellState = CellState.None;
     Image _cellImage => GetComponent<Image>();
+    /// <summary>周囲の地雷数 </summary>
     int _mineCount = 0;
+    int _row = 0;
+    int _column = 0;
     bool _isView = false;
     bool _isOpen = false;
+    bool _isMine = false;
+    /// <summary>自身に旗が立っているかどうか </summary>
     bool _isFlag = false;
+
     public CellState CellState
     {
         get => _cellState;
@@ -22,9 +30,12 @@ public class Cell : MonoBehaviour
         }
     }
 
+    
     public int MineCount { get => _mineCount; set => _mineCount = value; }
     public bool IsOpen { get => _isOpen; set => _isOpen = value; }
     public bool IsFlag { get => _isFlag; set => _isFlag = value; }
+    public int Row { get => _row; set => _row = value; }
+    public int Column { get => _column; set => _column = value; }
 
     void Start()
     {
@@ -36,7 +47,8 @@ public class Cell : MonoBehaviour
         OnCellStateChanged();
     }
 
-    void OnCellStateChanged()
+    /// <summary>セルの状態によって色を変更する </summary>
+    public void OnCellStateChanged()
     {
         if (_view == null) return;
 
@@ -56,6 +68,8 @@ public class Cell : MonoBehaviour
         }
     }
 
+    /// <summary>自身のセルを開ける </summary>
+    /// <returns>true=自身が地雷 false=地雷ではない</returns>
     public bool OpenCell()
     {
         _cellImage.enabled = false;
@@ -71,14 +85,29 @@ public class Cell : MonoBehaviour
         return false;
     }
 
+    public void OpenCell(Cell cell)
+    {
+        cell._cellImage.enabled = false;
+        cell.enabled = true;
+        _isOpen = true;
+    }
+
+    /// <summary>地雷が見えるようにする </summary>
+    /// <param name="isView"></param>
     public void SetView(bool isView)
     {
         _view.enabled = isView;
     }
+
+    /// <summary>状態をリセットする </summary>
+    public void ResetState()
+    {
+        CellState = CellState.None;
+        _mineCount = 0;
+    }
 }
 
-
-
+/// <summary>セルの状態</summary>
 public enum CellState
 {
     None = 0, // 空セル
--- Assets/MineSweeper/MineSweeper.cs
+++ Assets/MineSweeper/MineSweeper.cs
@@ -1,56 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+/// <summary>
+/// ゲームマネジャー 
+/// プレイヤーの入力を受け取る
+/// 盤面を生成する
+/// </summary>
 public class MineSweeper : MonoBehaviour
 {
-    [SerializeField] int _rows = 10;
-    [SerializeField] int _columns = 10;
-    [SerializeField] int _mineCount = 10;
-    [SerializeField] bool _isView = false;
+    [SerializeField, Tooltip("縦の長さ")] int _rows = 10;
+    [SerializeField, Tooltip("横の長さ")] int _columns = 10;
+    [SerializeField, Tooltip("地雷の数")] int _mineCount = 10;
+    [SerializeField, Tooltip("地雷のが見えるかどうか")] bool _isView = false;
     [SerializeField] GridLayoutGroup _gridLayoutGroup = null;
-    [SerializeField] Cell _cellPrefab = null;
-    [SerializeField] Text _timeText = null;
-
+    [SerializeField, Tooltip("セルのプレハブ")] Cell _cellPrefab = null;
+    [SerializeField, Tooltip("経過時間を表示するテキスト")] Text _timeText = null;
     int _currentFlagCount = 0;
     float _gameTime = 0;
     bool _isGame = true;
     bool _isTimerStart = false;
+    bool _isfirst = true;
     Cell[,] _cells;
+
     private void Start()
     {
         _gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
         _gridLayoutGroup.constraintCount = _columns;
         _cells = new Cell[_rows, _columns];
 
+        //盤面を生成する
         for (var r = 0; r < _rows; r++)
         {
             for (var c = 0; c < _columns; c++)
             {
                 var cell = Instantiate(_cellPrefab, _gridLayoutGroup.transform);
                 cell.SetView(_isView);
+                cell.Row = r;
+                cell.Column = c;
                 _cells[r, c] = cell;
             }
         }
 
-        for (var i = 0; i < _mineCount; i++)
-        {
-            var r = Random.Range(0, _rows);
-            var c = Random.Range(0, _columns);
-            var cell = _cells[r, c];
-
-            if (cell.CellState == CellState.None)
-            {
-                cell.CellState = CellState.Mine;
-                SetMineCount(r, c);
-            }
-            else
-            {
-                i--;
-            }
-        }
+        SetMine();
     }
 
     private void Update()
@@ -63,8 +54,20 @@ public class MineSweeper : MonoBehaviour
         {
             if (Input.GetButtonDown("Fire1") && hit)   //セルを開ける
             {
-                if (!_isTimerStart) _isTimerStart = true;
                 var cell = hit.collider.gameObject.GetComponent<Cell>();
+     
+                if (_isfirst)
+                {
+                    _isTimerStart = true;
+                    _isfirst = false;
+                    if (cell.CellState == CellState.Mine)
+                    {
+                        ResetMinePos();
+                        SetMine(cell);
+                    }
+                }
+
+                AroundCell(cell);
                 GameOver(cell.OpenCell());
                 GameClearCheck();
             }
@@ -88,14 +91,14 @@ public class MineSweeper : MonoBehaviour
                 GameClearCheck();
             }
 
-            if (_isTimerStart)
+            if (_isTimerStart)  //時間計測を開始する
             {
                 _gameTime += Time.deltaTime;
                 _timeText.text = _gameTime.ToString();
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))    //地雷の位置を見えるようにする
         {
             foreach (var cell in _cells)
             {
@@ -104,8 +107,15 @@ public class MineSweeper : MonoBehaviour
 
             _isView = !_isView;
         }
+
+        if (Input.GetKeyDown(KeyCode.C))    //リトライ
+        {
+            ResetMinePos();
+            SetMine();
+        }
     }
 
+    /// <summary>ゲームクリアしているから調べる </summary>
     private void GameClearCheck()
     {
         foreach (var cell in _cells)
@@ -129,6 +139,7 @@ public class MineSweeper : MonoBehaviour
         }
     }
 
+    /// <summary>時間計測を止める </summary>
     private void GameClear()
     {
         _isTimerStart = false;
@@ -136,6 +147,8 @@ public class MineSweeper : MonoBehaviour
         Debug.Log("クリア");
     }
 
+    /// <summary>地雷の引いたらゲームを終了させる </summary>
+    /// <param name="gameover">true=開けたセルが地雷である false=地雷ではない</param>
     private void GameOver(bool gameover)
     {
         if (gameover)
@@ -144,65 +157,172 @@ public class MineSweeper : MonoBehaviour
             _isGame = false;
         }
     }
-    
 
+    /// <summary>地雷をランダムにセットする </summary>
+    private void SetMine()
+    {
+        var count = Mathf.Min(_mineCount, _cells.Length);
+
+        for (var i = 0; i < count;)
+        {
+            var r = UnityEngine.Random.Range(0, _rows);
+            var c = UnityEngine.Random.Range(0, _columns);
+            var cell = _cells[r, c];
+
+            if (cell.CellState != CellState.Mine)　//選択されたセルの状態が空であれば地雷にする
+            {
+                i++;
+                cell.CellState = CellState.Mine;
+                SetMineCount(r, c);
+            }
+        }
+    }
+
+    /// <summary>プレイヤーが一手目に地雷を引いたら地雷を再セットする</summary>
+    private void SetMine(Cell firstCell)
+    {
+        for (var i = 0; i < _mineCount; i++)
+        {
+            var r = UnityEngine.Random.Range(0, _rows);
+            var c = UnityEngine.Random.Range(0, _columns);
+            var cell = _cells[r, c];
+
+            if (cell.CellState == CellState.None && firstCell != cell)
+            {
+                cell.CellState = CellState.Mine;
+                SetMineCount(r, c);
+            }
+            else
+            {
+                i--;
+            }
+        }
+    }
+
+    /// <summary>セルの状態を空の状態にする</summary>
+    private void ResetMinePos()
+    {
+        foreach (var cell in _cells)
+        {
+            cell.ResetState();
+        }
+    }
+    
+    /// <summary>地雷の数を数える </summary>
+    /// <param name="r"></param>
+    /// <param name="c"></param>
     private void SetMineCount(int r, int c)
     {
         if (0 <= r - 1 && 0 <= c - 1)   //左上
         {
-            var cell = _cells[r - 1, c - 1];
+            CheckMine(r - 1, c - 1);
+        }
+
+        if (0 <= r - 1) //上
+        {
+            CheckMine(r - 1, c);
+        }
+
+        if (0 <= r - 1 && c + 1 < _columns) //右上
+        {
+            CheckMine(r - 1, c + 1);
+        }
+
+        if (0 <= c - 1) //左
+        {
+            CheckMine(r, c - 1);
+        }
+
+        if (c + 1 < _columns)   //右
+        {
+            CheckMine(r, c + 1);
+        }
+
+
+        if (r + 1 < _rows && 0 <= c - 1) //左下
+        {
+            CheckMine(r + 1, c - 1);
+        }
+
+        if (r + 1 < _rows) //下
+        {
+            CheckMine(r + 1, c);
+        }
+
+        if (r + 1 < _rows && c + 1 < _columns) //右下
+        {
+            CheckMine(r + 1, c + 1);
+        }
+    }
+
+    /// <summary>引数を使いセルを取得しのそのセル状態が地雷ではなければ周囲の地雷の数を表示するにする</summary>
+    /// <param name="r"></param>
+    /// <param name="c"></param>
+    private void CheckMine(int r, int c)
+    {
+        var cell = _cells[r, c];
+        if (cell.CellState != CellState.Mine)
+        {
             cell.MineCount++;
             cell.CellState = CellState.Count;
+        }
+    }
+
+        /// <summary>開いたセルの周囲八マスを開ける </summary>
+        /// <param name="target"></param>
+    private void AroundCell(Cell target)
+    {
+        int r = target.Row;
+        int c = target.Column;
+
+        if (0 <= r - 1 && 0 <= c - 1)   //左上
+        {
+            var cell = _cells[r - 1, c - 1];
+            cell.OpenCell();
+            GameOver(cell.OpenCell());
         }
 
         if (0 <= r - 1) //上
         {
             var cell = _cells[r - 1, c];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
 
         if (0 <= r - 1 && c + 1 < _columns) //右上
         {
             var cell = _cells[r - 1, c + 1];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
 
         if (0 <= c - 1) //左
         {
             var cell = _cells[r, c - 1];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
 
         if (c + 1 < _columns)   //右
         {
             var cell = _cells[r, c + 1];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
 
 
         if (r + 1 < _rows && 0 <= c - 1) //左下
         {
             var cell = _cells[r + 1, c - 1];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
 
         if (r + 1 < _rows) //下
         {
             var cell = _cells[r + 1, c];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
 
         if (r + 1 < _rows && c + 1 < _columns) //右下
         {
             var cell = _cells[r + 1, c + 1];
-            cell.MineCount++;
-            cell.CellState = CellState.Count;
+            GameOver(cell.OpenCell());
         }
     }
 }
